eda67ee65b00649a79a45b693b7ac28e460bb70c
Victor <valad@gatech.edu>
2019-10-01 14:18:42 -0400

    merge two sorted array with O(1) space complexity completed

--- interesting_prob/space_opt_merge_seq.cpp
+++ interesting_prob/space_opt_merge_seq.cpp
@@ -21,13 +21,28 @@ void merge_seq(std::vector<int> &seq1, std::vector<int> &seq2)
             }
         }
     }
-    std::sort(seq2.begin(), seq2.end());
+    // std::sort(seq2.begin(), seq2.end());
+    for (int i = 0; i < seq2.size(); ++i)
+    {
+        for (int j = i; j < seq2.size(); ++j)
+        {
+            if (seq2[i] > seq2[j])
+            {
+                temp = seq2[i];
+                seq2[i] = seq2[j];
+                seq2[j] = temp;
+                // i = j;
+            }
+        }
+    }
 }
+
+// FT sensor needs to be calibrated with the weight and position of the added gripper, so with the weight of the gripper, it reads zero.
 
 int main()
 {
     std::vector<int> seq1{1, 3, 6, 7, 9, 10, 14, 18, 19};
-    std::vector<int> seq2{2, 3, 5, 8, 9, 11, 14, 15, 16, 17};
+    std::vector<int> seq2{2, 3, 5, 8, 9, 11, 14, 15};
 
     merge_seq(seq1, seq2);
 
