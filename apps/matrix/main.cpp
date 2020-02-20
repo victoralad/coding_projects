46254d80142abc0a82bd64fb59d0329a24831ef7
Victor <valad@gatech.edu>
2020-02-20 11:37:16 -0500

    Cleaned up main.cpp file

--- apps/matrix/main.cpp
+++ apps/matrix/main.cpp
@@ -4,32 +4,6 @@
 
 int main(int argc, char **argv)
 {
-    int nRows, nCols;
-    std::ifstream data_matrix;
-    data_matrix.open("Test1000.txt");
-    data_matrix >> nRows >> nCols;
-    std::vector <std::vector<double> > matrix(nRows, std::vector<double>(nCols, 0));
-    for (long long i = 0; i < nRows; i++)
-    {
-        for (long long j = 0; j < nCols; j++)
-        {
-            data_matrix >> matrix[i][j];
-        }
-    }
-    data_matrix.close();
-    Matrix matObjTest(matrix);
-    auto start = std::chrono::system_clock::now();
-
-    Matrix matObjTest2 = matObjTest.transpose();
-    // std::cout << matObjTest * matObjTest << std::endl;
-
-    auto end = std::chrono::system_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << elapsed.count() << '\n';
-    
-
-
-
     std::vector<std::vector<double> > mat1 = { {1, 2, 3}, 
                                                {4, 5, 6}, 
                                                {7, 8, 9}, 
@@ -53,7 +27,7 @@ int main(int argc, char **argv)
     // matObj = matObj1.transpose();
     // std::cout << matObj << std::endl;
 
-    // ------------ Test to check that multiplication with matrices of the wrong dimensions are not allowed -------------
+    // //------------ Test to check that multiplication with matrices of the wrong dimensions are not allowed -------------
     // std::cout << matObj1 * matObj3 << std::endl;     
 
     // std::cout << matObj1 * matObj2 << std::endl;
@@ -66,6 +40,37 @@ int main(int argc, char **argv)
 
     // // check to see that all the operations above have not affected the values of the original matrices
     // std::cout << matObj1 << "\n" << matObj2 << "\n" << matObj3 << std::endl;
+
+
+    // // ------------ The code below is used to evaluate the performance and correctness of the multihreaded part of the program --------
+    int nRows, nCols;
+
+    std::ifstream data_matrix;
+    data_matrix.open("Test1000.txt");
+
+    data_matrix >> nRows >> nCols;
+
+    std::vector <std::vector<double> > matrix(nRows, std::vector<double>(nCols, 0));
+    for (int i = 0; i < nRows; ++i)
+    {
+        for (int j = 0; j < nCols; ++j)
+        {
+            data_matrix >> matrix[i][j];
+        }
+    }
+    data_matrix.close();
+
+    Matrix matObjTest(matrix);
+
+    auto start = std::chrono::system_clock::now();
+
+    Matrix matObjTest2 = matObjTest.transpose();
+    std::cout << matObjTest << std::endl;
+
+    auto end = std::chrono::system_clock::now();
+
+    auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(end - start);  // elasped time in seconds
+    std::cout << elapsed.count() << '\n'; 
 
     return 0;
 }
